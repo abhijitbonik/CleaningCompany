17c0fd776f88728929fd4daa598e10c8c14f8268
shon <abhijitbonik@hotmail.com>
2019-09-13 20:34:09 +0530

    models added

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, redirect, url_for, flash, request
 from app.auth import bp
+from app import db, models
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
---
+++ app/models.py
@@ -0,0 +1,8 @@
+from app import db
+
+
+class Employee(db.Document):
+    email = db.StringField(required=True)
+    first_name = db.StringField(max_length=50)
+    last_name = db.StringField(max_length=50)
+
