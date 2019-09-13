86eaf31f7c9947270c94b1a6d562fa9a547c25e9
shon <abhijitbonik@hotmail.com>
2019-09-13 20:10:03 +0530

    mongo initialized

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,14 @@ from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from flask import Flask, request, current_app
 from config import Config
+from flask_mongoengine import MongoEngine
+
+db = MongoEngine()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    db.init_app(app)
     from app.auth import bp as auth_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
     return app
--- config.py
+++ config.py
@@ -6,7 +6,9 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    DATABASE_URI = os.environ.get('DATABASE_URL')+':'+os.environ.get('DATABASE_PORT')
-    DATABASE_DBNAME = os.environ.get('DATABASE_DBNAME')
-    DATABASE_USERNAME = os.environ.get('DATABASE_USERNAME')
-    DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD')
+    DEBUG = os.environ.get('DEBUG')
+    MONGODB_HOST = os.environ.get('MONGODB_HOST')
+    MONGODB_PORT = int(os.environ.get('MONGODB_PORT'))
+    MONGODB_DB = os.environ.get('MONGODB_DB')
+    MONGODB_USERNAME = os.environ.get('MONGODB_USERNAME')
+    MONGODB_PASSWORD = os.environ.get('MONGODB_PASSWORD')
