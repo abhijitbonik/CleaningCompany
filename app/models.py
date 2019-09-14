e02bdaa997f9369a33414f9663d498bdd76f3871
abhisgithub <abhijitbonik@hotmail.com>
2019-09-14 20:55:54 +0530

    models updated

--- app/models.py
+++ app/models.py
@@ -2,7 +2,50 @@ from app import db
 
 
 class Employee(db.Document):
-    email = db.StringField(required=True)
+    email = db.EmailField(required=True)
     first_name = db.StringField(max_length=50)
     last_name = db.StringField(max_length=50)
+    dob = db.DateTimeField()
+    identification=db.IntField()
+    phone = db.IntField()
+    availibility = db.BooleanField
+
+
+
+class Equipment(db.Document):
+    etype=db.StringField()
+    brand=db.StringField()
+    model=db.StringField()
+    remarks=db.StringField()
+    availibility=db.BooleanField()
+
+class Vehicle(db.Document):
+    title=db.StringField()
+    seats=db.IntField()
+    uid=db.StringField()
+    availibility=db.BooleanField()
+
+class Address(db.EmbeddedDocument):
+    addressLine1=db.StringField()
+    addressLine2=db.StringField()
+    landmark=db.StringField()
+    zipcode=db.StringField()
+    city=db.StringField()
+    state=db.StringField()
+
+class Jobs(db.Document):
+    customer_name=db.StringField()
+    job_address=db.ListField(db.EmbeddedDocumentField(Address))
+    billing_address=db.ListField(db.EmbeddedDocumentField(Address))
+    email=db.EmailField()
+    phone=db.IntField()
+    description=db.StringField()
+    size=db.StringField()
+    expectedStartDate=db.DateTimeField()
+    expectedEndDate=db.DateTimeField()
+    costPerWorker=db.FloatField()
+    assignedWorkers=db.ListField(db.ReferenceField(Employee))
+    costPerVehicle=db.FloatField()
+    assignedVehicles=db.ListField(db.ReferenceField(Vehicle))
+    cleaningEquipments=db.ListField(db.ReferenceField(Equipment))
 
