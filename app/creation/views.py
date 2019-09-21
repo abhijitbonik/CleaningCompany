1aa28a3f101bce4de5cd7c7bdb6cf1a96443fd18
abhisgithub <abhijitbonik@hotmail.com>
2019-09-21 17:38:18 +0530

    updated resource apis

--- app/creation/views.py
+++ app/creation/views.py
@@ -1,13 +1,14 @@
 from flask import Flask, request
 from flask_restful import Resource
 from app.models import db, Worker, Vehicle, Equipment
+from flask import jsonify
 
 class WorkerApi(Resource):
     def get(self, id=None):
         if id:
             worker = self.get_worker(id)
-            return worker.to_json() if worker else  {'status' : 'Worker not found'}
-        return Worker.objects().to_json()
+            return jsonify(worker) if worker else  {'status' : 'Worker not found'}
+        return jsonify(Worker.objects().exclude('id'))
 
     def post(self):
         if request.is_json:
@@ -19,7 +20,7 @@ class WorkerApi(Resource):
             phone = request.json['phone']
             worker=Worker(email=email, first_name=fname, last_name=lname, dob=dob, identification=identity, phone=phone, availibility=True)
             worker.save()
-            return worker.to_json()
+            return jsonify(worker)
         return {'status': 'invalid request'}
 
     def put(self, id):
@@ -36,8 +37,10 @@ class WorkerApi(Resource):
 
     def get_worker(self, id):
         try:
-            return Worker.objects.get(identification=id)
+            return Worker.objects.exclude('id').get(identification=id)
         except Worker.DoesNotExist:
+            return None
+        except Worker.MultipleObjectsReturned:
             return None
 
 
@@ -45,8 +48,8 @@ class VehicleApi(Resource):
     def get(self, id=None):
         if id:
             vehicle = self.get_vehicle(id)
-            return vehicle.to_json() if vehicle else  {'status' : 'Vehicle not found'}
-        return Vehicle.objects().to_json()
+            return jsonify(vehicle) if vehicle else  {'status' : 'Vehicle not found'}
+        return jsonify(Vehicle.objects().exclude('id'))
 
     def post(self):
         if request.is_json:
@@ -55,7 +58,7 @@ class VehicleApi(Resource):
             uid = request.json['uid']
             vehicle=Vehicle(name=name, seats=seats, uid=uid, availibility=True)
             vehicle.save()
-            return vehicle.to_json()
+            return jsonify(vehicle)
         return {'status': 'invalid request'}
 
     def put(self, id):
@@ -72,7 +75,7 @@ class VehicleApi(Resource):
 
     def get_vehicle(self, id):
         try:
-            return Vehicle.objects.get(uid=id)
+            return Vehicle.objects.exclude('id').get(uid=id)
         except Vehicle.DoesNotExist:
             return None
 
@@ -81,8 +84,8 @@ class EquipmentApi(Resource):
     def get(self, id=None):
         if id:
             equp = self.get_equipment(id)
-            return equp.to_json() if equp else  {'status' : 'Equipment not found'}
-        return Equipment.objects().to_json()
+            return jsonify(equp) if equp else  {'status' : 'Equipment not found'}
+        return jsonify(Equipment.objects().exclude('id'))
 
     def post(self):
         if request.is_json:
@@ -93,7 +96,7 @@ class EquipmentApi(Resource):
             remarks = request.json['remarks']
             equp=Equipment(uid=uid, etype=etype, brand=brand, model=model, remarks=remarks, availibility=True)
             equp.save()
-            return equp.to_json()
+            return jsonify(equp)
         return {'status': 'invalid request'}
 
     def delete(self, id):
@@ -110,6 +113,6 @@ class EquipmentApi(Resource):
 
     def get_equipment(self, id):
         try:
-            return Equipment.objects.get(uid=id)
+            return Equipment.objects.exclude('id').get(uid=id)
         except Equipment.DoesNotExist:
             return None
