d17961f8e03ff7f5de3c93b5878749f77ad38bcc
abhisgithub <abhijitbonik@hotmail.com>
2019-10-03 23:17:32 +0530

    updated

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -2,5 +2,4 @@ from app.auth import api
 from app.auth.views import UserLogin, UserRegistration
 
 api.add_resource(UserLogin, '/login')
-api.add_resource(UserRegistration, '/register')
 
--- app/auth/views.py
+++ app/auth/views.py
@@ -6,7 +6,6 @@ from flask_jwt_extended import create_access_token, create_refresh_token
 parser = reqparse.RequestParser()
 parser.add_argument('username', help = 'This field cannot be blank', required = True)
 parser.add_argument('password', help = 'This field cannot be blank', required = True)
-parser.add_argument('email', help = 'This field cannot be blank')
 
 class UserLogin(Resource):
     def post(self):
@@ -27,30 +26,4 @@ class UserLogin(Resource):
         else:
             return {'msg': 'Wrong credentials'}
 
-class UserRegistration(Resource):
-    def post(self):
-        data = parser.parse_args()
-        try:
-            User.objects.get(username=data['username'])
-            return {'msg': 'User {} already exist'.format(data['username'])}
-        except User.DoesNotExist:
-            pass
-        
-        new_user = User(
-            email = data['email'],
-            username = data['username'],
-            password = User.generate_hash(data['password'])
-        )
-        
-        try:
-            new_user.save()
-            access_token = create_access_token(identity = data['username'])
-            refresh_token = create_refresh_token(identity = data['username'])
-            return {
-                'msg': 'User {} was created'.format(data['username']),
-                'access_token': access_token,
-                'refresh_token': refresh_token
-                }
-        except:
-            return {'msg': 'Something went wrong'}, 500
 
